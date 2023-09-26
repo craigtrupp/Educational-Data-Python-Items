f1b63589ac9332bf7d21b0e69c5c4481aa8c2dac
Craig Rupp <craigtrupp8@gmail.com>
2023-09-26 12:47:06 -0500

    String capitalize

--- HackerRank/entry_easy/third_set.py
+++ HackerRank/entry_easy/third_set.py
@@ -334,3 +334,31 @@ def print_rangoli(size):
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
+
+
+#### **Strings Capitalize** ####
+# You are asked to ensure that the first and last names of people begin with a capital letter in their passports. 
+# For example, alison heck should be capitalised correctly as Alison Heck.
+# * alison heck => Alison Heck
+# Given a full name, your task is to capitalize the name appropriately.
+# Input Format
+# A single line of input containing the full name, S.
+#!/bin/python3
+
+import os
+
+# Complete the solve function below.
+def solve(s):
+    # Would "title" any character in string based off of whitespace
+    return ' '.join([s.title() for s in s.split()])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
